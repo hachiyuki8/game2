f7fa9974a8cfe34c2f2ffa82fd7726c44aa2f32d
hachiyuki8 <caladuil@outlook.com>
2022-09-12 20:47:18 -0400

    add models and board rendering

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -10,50 +10,138 @@
 
 #include <glm/gtc/type_ptr.hpp>
 
+// for glm::to_stirng();
+#include <glm/gtx/string_cast.hpp>
+
 #include <random>
 
-GLuint hexapod_meshes_for_lit_color_texture_program = 0;
-Load< MeshBuffer > hexapod_meshes(LoadTagDefault, []() -> MeshBuffer const * {
-	MeshBuffer const *ret = new MeshBuffer(data_path("hexapod.pnct"));
-	hexapod_meshes_for_lit_color_texture_program = ret->make_vao_for_program(lit_color_texture_program->program);
+const std::string PIZZA = "Pizza";
+const std::string SAUSAGE = "Sausage";
+const std::string PEPPER = "Pepper";
+const std::string PEPPERONI = "Pepperoni";
+const std::string OLIVE = "Olive";
+
+const std::string CAKE = "Cake";
+const std::string CHOCOLATE = "Chocolate";
+const std::string KIWI = "Kiwi";
+
+const std::string CHUNK = "Chunk";
+const std::string HIGHLIGHT = "Highlight";
+
+GLuint board_meshes_for_lit_color_texture_program = 0;
+Load< MeshBuffer > board_meshes(LoadTagDefault, []() -> MeshBuffer const * {
+	MeshBuffer const *ret = new MeshBuffer(data_path("pizza.pnct"));
+	board_meshes_for_lit_color_texture_program = ret->make_vao_for_program(lit_color_texture_program->program);
 	return ret;
 });
 
-Load< Scene > hexapod_scene(LoadTagDefault, []() -> Scene const * {
-	return new Scene(data_path("hexapod.scene"), [&](Scene &scene, Scene::Transform *transform, std::string const &mesh_name){
-		Mesh const &mesh = hexapod_meshes->lookup(mesh_name);
+Load< Scene > board_scene(LoadTagDefault, []() -> Scene const * {
+	return new Scene(data_path("pizza.scene"), [&](Scene &scene, Scene::Transform *transform, std::string const &mesh_name){
+		Mesh const &mesh = board_meshes->lookup(mesh_name);
+		if (DEBUG) {
+			std::cout << transform->name << " " << mesh_name << std::endl;
+		}
 
 		scene.drawables.emplace_back(transform);
 		Scene::Drawable &drawable = scene.drawables.back();
 
 		drawable.pipeline = lit_color_texture_program_pipeline;
 
-		drawable.pipeline.vao = hexapod_meshes_for_lit_color_texture_program;
+		drawable.pipeline.vao = board_meshes_for_lit_color_texture_program;
 		drawable.pipeline.type = mesh.type;
 		drawable.pipeline.start = mesh.start;
 		drawable.pipeline.count = mesh.count;
-
 	});
 });
 
-PlayMode::PlayMode() : scene(*hexapod_scene) {
+PlayMode::PlayMode() : scene(*board_scene) {
 	//get pointers to leg for convenience:
-	for (auto &transform : scene.transforms) {
-		if (transform.name == "Hip.FL") hip = &transform;
-		else if (transform.name == "UpperLeg.FL") upper_leg = &transform;
-		else if (transform.name == "LowerLeg.FL") lower_leg = &transform;
-	}
-	if (hip == nullptr) throw std::runtime_error("Hip not found.");
-	if (upper_leg == nullptr) throw std::runtime_error("Upper leg not found.");
-	if (lower_leg == nullptr) throw std::runtime_error("Lower leg not found.");
+	// for (auto &transform : scene.transforms) {
+	// 	if (transform.name == "Hip.FL") hip = &transform;
+	// 	else if (transform.name == "UpperLeg.FL") upper_leg = &transform;
+	// 	else if (transform.name == "LowerLeg.FL") lower_leg = &transform;
+	// }
+	// if (hip == nullptr) throw std::runtime_error("Hip not found.");
+	// if (upper_leg == nullptr) throw std::runtime_error("Upper leg not found.");
+	// if (lower_leg == nullptr) throw std::runtime_error("Lower leg not found.");
 
-	hip_base_rotation = hip->rotation;
-	upper_leg_base_rotation = upper_leg->rotation;
-	lower_leg_base_rotation = lower_leg->rotation;
+	// hip_base_rotation = hip->rotation;
+	// upper_leg_base_rotation = upper_leg->rotation;
+	// lower_leg_base_rotation = lower_leg->rotation;
 
 	//get pointer to camera for convenience:
 	if (scene.cameras.size() != 1) throw std::runtime_error("Expecting scene to have exactly one camera, but it has " + std::to_string(scene.cameras.size()));
 	camera = &scene.cameras.front();
+
+	// initialize game state
+	for (auto &drawable : scene.drawables) {
+		if (drawable.transform->name == CHUNK) {
+			chunk_pipeline.type = drawable.pipeline.type;
+			chunk_pipeline.start = drawable.pipeline.start;
+			chunk_pipeline.count = drawable.pipeline.count;
+		} else if (drawable.transform->name == HIGHLIGHT) {
+			highlight_pipeline.type = drawable.pipeline.type;
+			highlight_pipeline.start = drawable.pipeline.start;
+			highlight_pipeline.count = drawable.pipeline.count;
+		} else if (drawable.transform->name == PIZZA) {
+			pizza_pipeline.type = drawable.pipeline.type;
+			pizza_pipeline.start = drawable.pipeline.start;
+			pizza_pipeline.count = drawable.pipeline.count;
+		} else if (drawable.transform->name == SAUSAGE) {
+			sausage_pipeline.type = drawable.pipeline.type;
+			sausage_pipeline.start = drawable.pipeline.start;
+			sausage_pipeline.count = drawable.pipeline.count;
+		} else if (drawable.transform->name == PEPPER) {
+			pepper_pipeline.type = drawable.pipeline.type;
+			pepper_pipeline.start = drawable.pipeline.start;
+			pepper_pipeline.count = drawable.pipeline.count;
+		} else if (drawable.transform->name == PEPPERONI) {
+			pepperoni_pipeline.type = drawable.pipeline.type;
+			pepperoni_pipeline.start = drawable.pipeline.start;
+			pepperoni_pipeline.count = drawable.pipeline.count;
+		} else if (drawable.transform->name == OLIVE) {
+			olive_pipeline.type = drawable.pipeline.type;
+			olive_pipeline.start = drawable.pipeline.start;
+			olive_pipeline.count = drawable.pipeline.count;
+		} else if (drawable.transform->name == CAKE) {
+			cake_pipeline.type = drawable.pipeline.type;
+			cake_pipeline.start = drawable.pipeline.start;
+			cake_pipeline.count = drawable.pipeline.count;
+		} 
+	}
+
+	scene.drawables.clear();
+	highlight_transform = new Scene::Transform;
+	highlight_transform->name = "Highlight";
+	highlight_transform->position = glm::vec3(0.0f, 0.0f, 0.0f);
+	highlight_transform->scale = glm::vec3(1.0f, 1.0f, 1.0f);
+	scene.drawables.emplace_back(highlight_transform);
+
+	Scene::Drawable &drawable = scene.drawables.back();
+	drawable.pipeline = lit_color_texture_program_pipeline;
+	drawable.pipeline.vao = board_meshes_for_lit_color_texture_program;
+	drawable.pipeline.type = highlight_pipeline.type;
+	drawable.pipeline.start = highlight_pipeline.start;
+	drawable.pipeline.count = highlight_pipeline.count;
+	for (uint16_t r = 0; r < num_rows; r++) {
+		for (uint16_t c = 0; c < num_cols; c++) {
+			chunk_transforms[r*SIZE+c] = new Scene::Transform;
+			chunk_transforms[r*SIZE+c]->name = "Chunk " + std::to_string(r*SIZE+c);
+			chunk_transforms[r*SIZE+c]->position = glm::vec3(-chunk_size*r, -chunk_size*c, -1.0f);
+			chunk_transforms[r*SIZE+c]->scale = glm::vec3(1.0f, 1.0f, 0.2f);
+			scene.drawables.emplace_back(chunk_transforms[r*SIZE+c]);
+
+			Scene::Drawable &drawable = scene.drawables.back();
+			drawable.pipeline = lit_color_texture_program_pipeline;
+			drawable.pipeline.vao = board_meshes_for_lit_color_texture_program;
+			drawable.pipeline.type = chunk_pipeline.type;
+			drawable.pipeline.start = chunk_pipeline.start;
+			drawable.pipeline.count = chunk_pipeline.count;
+
+			board[r*SIZE+c] = false;
+		}
+	}
+	highlight_transform->parent = chunk_transforms[cur_row*SIZE+cur_col];
 }
 
 PlayMode::~PlayMode() {
@@ -80,6 +168,26 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 		} else if (evt.key.keysym.sym == SDLK_s) {
 			down.downs += 1;
 			down.pressed = true;
+			return true;
+		} else if (evt.key.keysym.sym == SDLK_UP) {
+			cur_row = std::max(0, cur_row - 1);
+			return true;
+		} else if (evt.key.keysym.sym == SDLK_DOWN) {
+			cur_row = std::min(num_rows - 1, cur_row + 1);
+			return true;
+		} else if (evt.key.keysym.sym == SDLK_LEFT) {
+			cur_col = std::max(0, cur_col - 1);
+			return true;
+		} else if (evt.key.keysym.sym == SDLK_RIGHT) {
+			cur_col = std::min(num_cols - 1, cur_col + 1);
+			return true;
+		} else if (evt.key.keysym.sym == SDLK_RETURN) {
+			if (drop_piece_on_board()) {
+				generate_new_piece();
+			}
+			return true;
+		} else if (evt.key.keysym.sym == SDLK_x) {
+			generate_new_piece();
 			return true;
 		}
 	} else if (evt.type == SDL_KEYUP) {
@@ -119,27 +227,65 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 	return false;
 }
 
+bool PlayMode::drop_piece_on_board() {
+	if (board[cur_row*SIZE+cur_col]) {
+		if (DEBUG) {
+			std::cout << "[logger] (" << cur_row << "," << cur_col << ") occupied" << std::endl;
+		}
+		return false; // current position already occupied
+	}
+
+	// TODO: check constraints
+	board[cur_row*SIZE+cur_col] = true;
+	board_content[cur_row*SIZE+cur_col] = cur_piece;
+	pieces_dropped += 1;
+	update_score();
+	return true;
+}
+
+void PlayMode::generate_new_piece() {
+	if (pieces_remaining == 0) {
+		gameState = GameState::END;
+		if (DEBUG) {
+			std::cout << "[logger] no more pieces remaining" << std::endl;
+		}	
+		return;
+	} 
+
+	// TODO: generate a new random piece
+	pieces_remaining -= 1;
+}
+
+void PlayMode::update_score() {
+	// TODO: check patterns
+	if (DEBUG) {
+		std::cout << "[progress] score: " << cur_score << "|occupied: " << pieces_dropped << "|remaining: " << pieces_remaining << std::endl;
+	}	
+	return;
+}
+
 void PlayMode::update(float elapsed) {
 
 	//slowly rotates through [0,1):
-	wobble += elapsed / 10.0f;
-	wobble -= std::floor(wobble);
+	// wobble += elapsed / 10.0f;
+	// wobble -= std::floor(wobble);
 
-	hip->rotation = hip_base_rotation * glm::angleAxis(
-		glm::radians(5.0f * std::sin(wobble * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 1.0f, 0.0f)
-	);
-	upper_leg->rotation = upper_leg_base_rotation * glm::angleAxis(
-		glm::radians(7.0f * std::sin(wobble * 2.0f * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 0.0f, 1.0f)
-	);
-	lower_leg->rotation = lower_leg_base_rotation * glm::angleAxis(
-		glm::radians(10.0f * std::sin(wobble * 3.0f * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 0.0f, 1.0f)
-	);
+	// hip->rotation = hip_base_rotation * glm::angleAxis(
+	// 	glm::radians(5.0f * std::sin(wobble * 2.0f * float(M_PI))),
+	// 	glm::vec3(0.0f, 1.0f, 0.0f)
+	// );
+	// upper_leg->rotation = upper_leg_base_rotation * glm::angleAxis(
+	// 	glm::radians(7.0f * std::sin(wobble * 2.0f * 2.0f * float(M_PI))),
+	// 	glm::vec3(0.0f, 0.0f, 1.0f)
+	// );
+	// lower_leg->rotation = lower_leg_base_rotation * glm::angleAxis(
+	// 	glm::radians(10.0f * std::sin(wobble * 3.0f * 2.0f * float(M_PI))),
+	// 	glm::vec3(0.0f, 0.0f, 1.0f)
+	// );
 
-	//move camera:
-	{
+	highlight_transform->parent = chunk_transforms[cur_row*SIZE+cur_col];
+
+	{ // move camera
 
 		//combine inputs into a move:
 		constexpr float PlayerSpeed = 30.0f;
@@ -188,6 +334,13 @@ void PlayMode::draw(glm::uvec2 const &drawable_size) {
 
 	GL_ERRORS(); //print any errors produced by this setup code
 
+	// cake->transform->parent = chunk->transform;
+	// cake->transform->position = glm::vec3(2.0f, 2.0f, 2.0f);
+	// std::cout << glm::to_string(cake->transform->position) << std::endl;
+	// std::cout << glm::to_string(chunk->transform->position) << std::endl;
+	// for (auto const &drawable : scene.drawables) {
+	// 	std::cout << drawable.transform->name << " " << glm::to_string(drawable.transform->position) << std::endl;
+	// }
 	scene.draw(*camera);
 
 	{ //use DrawLines to overlay some text:
--- PlayMode.hpp
+++ PlayMode.hpp
@@ -7,6 +7,38 @@
 #include <vector>
 #include <deque>
 
+const bool DEBUG = true;
+
+struct Pizza {
+	bool pepperoni;
+	bool black_olive;
+	bool pepper;
+	bool sausage;
+};
+
+struct Cake {
+	bool chocolate;
+	bool kiwi;
+	bool strawberry;
+	bool marshmellow;
+};
+
+struct Piece {
+	bool isPizza = true;
+	union {
+		struct Pizza pizza;
+		struct Cake cake;
+	} content;
+};
+
+struct PipelineInfo {
+	GLenum type = GL_TRIANGLES;
+	GLuint start = 0;
+	GLuint count = 0; 
+};
+
+const uint16_t SIZE = 8;
+
 struct PlayMode : Mode {
 	PlayMode();
 	virtual ~PlayMode();
@@ -27,14 +59,53 @@ struct PlayMode : Mode {
 	//local copy of the game scene (so code can change it during gameplay):
 	Scene scene;
 
+	PipelineInfo chunk_pipeline;
+	PipelineInfo highlight_pipeline;
+	PipelineInfo pizza_pipeline;
+	PipelineInfo sausage_pipeline;
+	PipelineInfo pepper_pipeline;
+	PipelineInfo pepperoni_pipeline;
+	PipelineInfo olive_pipeline;
+	PipelineInfo cake_pipeline;
+
+	Scene::Transform *chunk_transforms[SIZE*SIZE];
+	Scene::Transform *highlight_transform;
+
+	enum GameState : uint16_t {
+		START,
+		IN_PROGRESS,
+		END,
+	};
+	GameState gameState = GameState::START;
+
+	//board
+	uint16_t num_rows = SIZE;
+	uint16_t num_cols = SIZE;
+	uint16_t cur_row = 0;
+	uint16_t cur_col = 0;
+	float chunk_size = 2.0f;
+
+	bool board[SIZE*SIZE];
+	Piece board_content[SIZE*SIZE];
+
+	Piece cur_piece;
+	uint16_t pieces_dropped = 0;
+	uint16_t pieces_remaining = 100;
+	uint16_t cur_score = 0;
+	// drop the current piece on the selected position on the board if it's valid
+	virtual bool drop_piece_on_board();
+	virtual void update_score();
+	// generate a new piece of pizza or cake if there's any chance left
+	virtual void generate_new_piece();
+
 	//hexapod leg to wobble:
-	Scene::Transform *hip = nullptr;
-	Scene::Transform *upper_leg = nullptr;
-	Scene::Transform *lower_leg = nullptr;
-	glm::quat hip_base_rotation;
-	glm::quat upper_leg_base_rotation;
-	glm::quat lower_leg_base_rotation;
-	float wobble = 0.0f;
+	// Scene::Transform *hip = nullptr;
+	// Scene::Transform *upper_leg = nullptr;
+	// Scene::Transform *lower_leg = nullptr;
+	// glm::quat hip_base_rotation;
+	// glm::quat upper_leg_base_rotation;
+	// glm::quat lower_leg_base_rotation;
+	// float wobble = 0.0f;
 	
 	//camera:
 	Scene::Camera *camera = nullptr;
